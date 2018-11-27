78c9c61130ea9709a37612dfedce81970cd6b03e
daiaji <daiaji@suse-home.lan>
2018-11-27 18:18:30 +0800

    first commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Magisk Module Template
+# 修改MOTO Z²的U-Touch按键设置
 
-**Update `README.md` if you want to submit your module to the online repo!** This `README.md` will be shown in a Webview dialog when a user taps your module in Magisk Manager, so make sure to place some information / changelog / notes here.
+ZUI还好 ，能设置按键功能，海外版就很蛋疼了
 
-If you are not familiar with the Markdown syntax, you can start by experimenting on GitHub's online Markdown editor, which will let you preview before publishing. If you need more help, the [Markdown Cheat Sheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) will be handy.
+还好有[MYLF写的这篇教程](https://bbs.ixmoe.com/thread-2399-1-1.html)
 
-For more information about modules and repos, please check the [official documentation](https://topjohnwu.github.io/Magisk/)
+而我姑且做了这个最简易的Magisk实现
--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "     让你的MOTO更好用-MOTO Z²Force U-Touch按键修改    "
   ui_print "*******************************"
 }
 
@@ -63,6 +63,7 @@ REPLACE="
 # Construct your own list here, it will override the example above
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
+/system/usr/keylayout/uinput-fpc.kl
 "
 
 ##########################################################################################
@@ -85,6 +86,8 @@ set_permissions() {
 
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
+  set_perm  $MODPATH/system/usr/keylayout/uinput-fpc.kl  0  0  0644
+
 }
 
 ##########################################################################################
